6e169e03cc18a418121b73ecce7ad45c86675fa8
TanyaRan <tatyanaran@gmail.com>
2019-04-10 15:37:43 +0300

    Updates

---
+++ precache-manifest.76168e2784e03c5ab3321b3f707c491c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/TheVenue_React/static/media/slide_two.3a3ffd36.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/TheVenue_React/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/TheVenue_React/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "61919a89fd1f7e4d22b3",
+    "url": "/TheVenue_React/static/js/runtime~main.401d6b1c.js"
+  },
+  {
+    "revision": "01348d8c9890939c1107",
+    "url": "/TheVenue_React/static/js/main.ad3d147b.chunk.js"
+  },
+  {
+    "revision": "c306faeb89dc4271922c",
+    "url": "/TheVenue_React/static/js/2.9f2672d2.chunk.js"
+  },
+  {
+    "revision": "01348d8c9890939c1107",
+    "url": "/TheVenue_React/static/css/main.5a9aaf89.chunk.css"
+  },
+  {
+    "revision": "3e3938aedbf75a1041b8560b88be6aed",
+    "url": "/TheVenue_React/index.html"
+  }
+];
